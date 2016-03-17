1dbbfb8ceb458c9a6cd117cd4bce214ddf389bee
lj50036 <jl52741@gmail.com>
2016-03-17 10:16:50 -0500

    Make this build in an Omni tree

--- omni_y550.mk
+++ omni_y550.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/huawei/y550/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := y550
-PRODUCT_NAME := cm_y550
+PRODUCT_NAME := omni_y550
 PRODUCT_BRAND := huawei
 PRODUCT_MANUFACTURER := Huawei
 PRODUCT_MODEL := Y550-L01
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_y550-userdebug
-add_lunch_combo cm_y550-eng
+add_lunch_combo omni_y550-userdebug
+add_lunch_combo omni_y550-eng
